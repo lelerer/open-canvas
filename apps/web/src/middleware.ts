637f182cc74f290764dbd8354ac16719f0921183
lelerer <lele.rer@outlook.com>
2026-06-20 02:24:32 +0800

    Update middleware.ts

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,12 +1,22 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/lib/supabase/middleware";
+// import { type NextRequest } from "next/server";
+// import { updateSession } from "@/lib/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request);
+// }
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+//   ],
+// };
+
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(_request: NextRequest) {
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: [],
 };
